78dad278fa7d02b8bb46549f00e5bc7fba23a8ed
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2022-06-28 11:40:32 +0530

    resolved issues

--- index.js
+++ index.js
@@ -4,67 +4,58 @@ const bodyParser = require('body-parser')
 //Initialize express app
 const app = express();
 
-const Domain = require('./model/models')
-const Subdomain = require('./model/models');
+const { Domain, SubDomain } = require('./model/models')
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-mongoose.connect('mongodb+srv://aditikashyap:C8BulNyeCYnGbeQS@nodetuts.0ugnzoe.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://aditikashyap:C8BulNyeCYnGbeQS@nodetuts.0ugnzoe.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-    console.log('connected to db')
+        console.log('connected to db')
     })
     .catch((error) => {
- console.log(error)
+        console.log(error)
     });
 
-    // Creating a new book
+// Creating a new book
 
-    app.post('/', (req, res) => {
-        
-        console.log(req.body);
+app.post('/', (req, res) => {
+    let domainName = req.body.domainName;
+    let info_domain = req.body.info_domain;
+    let companies_domain = req.body.companies_domain;
+    let roles_domain = req.body.roles_domain;
+    let day_in_the_life_domain = req.body.day_in_the_life_domain;
+    let competitions_domain = req.body.competitions_domain;
+    let internship_domain = req.body.internship_domain;
+    let research_opportunities_domain = req.body.research_opportunities_domain;
+    let subdomainName = req.body.subdomainName;
+    let info_subdomain = req.body.info_subdomain;
+    let companies_subdomain = req.body.companies_subdomain;
+    let roles_subdomain = req.body.roles_subdomain;
+    let day_in_the_life_subdomain = req.body.day_in_the_life_subdomain;
+    let competitions_subdomain = req.body.competitions_subdomain;
+    let internship_subdomain = req.body.internship_subdomain;
+    let research_opportunities_subdomain = req.body.research_opportunities_subdomain;
+    let newDomain = Domain({
+        domainName: domainName,
+        subdomain: { subdomainName, info_subdomain, companies_subdomain, roles_subdomain, day_in_the_life_subdomain, competitions_subdomain, internship_subdomain, research_opportunities_subdomain },
+        info_domain: info_domain,
+        companies_domain: companies_domain,
+        roles_domain: roles_domain,
+        day_in_the_life_domain: day_in_the_life_domain,
+        competitions_domain: competitions_domain,
+        internship_domain: internship_domain,
+        research_opportunities_domain: research_opportunities_domain
+    })
+    newDomain.save((err, domain) => {
+        console.log({ domain, err })
+    })
+    //    let subdomain;
 
-            let domainName = req.body.domainName;
-            let  info_domain =req.body.info_domain;
-            let companies_domain = req.body.companies_domain;
-            let roles_domain = req.body.roles_domain;
-            let day_in_the_life_domain = req.body.day_in_the_life_domain;
-            let competitions_domain =  req.body.competitions_domain;
-            let  internship_domain = req.body.internship_domain;
-            let research_opportunities_domain = req.body.research_opportunities_domain;
-            let subdomainName =  req.body.subdomainName;
-            let info_subdomain = req.body.info_subdomain;
-            let companies_subdomain = req.body.companies_subdomain;
-            let roles_subdomain = req.body.roles_subdomain;
-            let day_in_the_life_subdomain = req.body.day_in_the_life_subdomain;
-            let competitions_subdomain = req.body.competitions_subdomain;
-            let internship_subdomain = req.body.internship_subdomain;
-            let research_opportunities_subdomain = req.body.research_opportunities_subdomain;
 
-    
-    
-
-
-        let newDomain = new Domain({
-            domainName: domainName,
-            subdomain : {subdomainName, info_subdomain, companies_subdomain, roles_subdomain, day_in_the_life_subdomain, competitions_subdomain, internship_subdomain, research_opportunities_subdomain},
-            info_domain: info_domain,
-           companies_domain : companies_domain,
-           roles_domain : roles_domain,
-           day_in_the_life_domain : day_in_the_life_domain,
-           competitions_domain : competitions_domain,
-           internship_domain : internship_domain,
-           research_opportunities_domain :research_opportunities_domain
-           })
-           
-           newDomain.save()
-
-        //    let subdomain;
-           
-
-        // let {subdomainName, info_subdomain, companies_subdomain, roles_subdomain, day_in_the_life_subdomain, competitions_subdomain, internship_subdomain, research_opportunities_subdomain} = subdomain;
+    // let {subdomainName, info_subdomain, companies_subdomain, roles_subdomain, day_in_the_life_subdomain, competitions_subdomain, internship_subdomain, research_opportunities_subdomain} = subdomain;
 
 
     //    let newSubdomain = new Subdomain({
@@ -79,47 +70,38 @@ mongoose.connect('mongodb+srv://aditikashyap:C8BulNyeCYnGbeQS@nodetuts.0ugnzoe.m
     //      })
 
     //     newSubdomain.save()
-
-        .then((Domain) => {
-            res.send(Domain)
-           })
-           .catch((err) => {
-            console.log(err)
-           })
-
-            
-       });
+});
 
 
 
-       app.get('/', (req,res) =>{
-           Domain.find()
-            .then(domain => {
-                res.send(domain);
-            })
-            .catch(err =>{
-                res.status(500).send({
-                    message: err.message || "Some error has occured."
-                });
+app.get('/', (req, res) => {
+    Domain.find()
+        .then(domain => {
+            res.send(domain);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error has occured."
             });
-           
-       });
-    // Reading an entry from Book
+        });
 
-    app.get('/:id', (req, res) =>{
-    Domain.findById(req.params.id, (err, user) =>{
-     res.send(user)
+});
+// Reading an entry from Book
+
+app.get('/:id', (req, res) => {
+    Domain.findById(req.params.id, (err, user) => {
+        res.send(user)
     })
-   });
+});
 
 
-   // Updating the entry
+// Updating the entry
 // app.put('/update/:id', (req, res) => {
 //     let domain = {}
 //     if (req.body.domainName) domain.domainName = req.body.domainName
 //     if (req.body.description) domain.description = req.body.description
 //     if (req.body.imgLink) domain.imgLink = req.body.imgLink
-    
+
 //    domain = { $set: domain }
 //    Domain.updateOne({_id: req.params.id}, domain)
 //    .then(() => {
@@ -129,20 +111,20 @@ mongoose.connect('mongodb+srv://aditikashyap:C8BulNyeCYnGbeQS@nodetuts.0ugnzoe.m
 //     })
 //    }),
 
-   // Deleting the entry from Book
+// Deleting the entry from Book
 app.delete('/delete/:id', (req, res) => {
     Domain.findByIdAndRemove(req.params.id)
-    .then(() => {
-     res.send('user deleted')
-    }).catch((err) => {
-     console.log(err)
-    })
-   });
+        .then(() => {
+            res.send('user deleted')
+        }).catch((err) => {
+            console.log(err)
+        })
+});
 
 
 //Initialize the sever
 const port = process.env.PORT || 9090;
 
 app.listen(port, () => {
-    console.log('sever listening on port:'+ port);
+    console.log('sever listening on port:' + port);
 });
--- model/models.js
+++ model/models.js
@@ -2,83 +2,82 @@ const mongoose = require('mongoose');
 
 // Schema for AddressBook
 const subdomainSchema = new mongoose.Schema({
-    subdomainName :{
-        type : String,
+    subdomainName: {
+        type: String,
         required: false
-     },
-     info_subdomain : {
-         type : String,
-         required : false
-     },
-     companies_subdomain : {
-         type : String,
-         required : false
-     },
-     roles_subdomain : {
-         type : String,
-         required : false
-     },
-     day_in_the_life_subdomain : {
-         type : String,
-         required : false
-     },
-     competitions_subdomain : {
-         type : String,
-         required : false
-     },
-     internships_subdomain :{
-         type : String,
-         required : false
-     },
-     research_opportunities_subdomain : {
-         type : String,
-         required : false
-     }
- 
+    },
+    info_subdomain: {
+        type: String,
+        required: false
+    },
+    companies_subdomain: {
+        type: String,
+        required: false
+    },
+    roles_subdomain: {
+        type: String,
+        required: false
+    },
+    day_in_the_life_subdomain: {
+        type: String,
+        required: false
+    },
+    competitions_subdomain: {
+        type: String,
+        required: false
+    },
+    internships_subdomain: {
+        type: String,
+        required: false
+    },
+    research_opportunities_subdomain: {
+        type: String,
+        required: false
+    }
+
 });
 const domainSchema = new mongoose.Schema({
     domainName: {
-     type: String,
-     required: true,
-     unique : true,
+        type: String,
+        required: true,
+        unique: true,
     },
-    subdomain :{
-        type : subdomainSchema,
-        required : false
+    subdomain: {
+        type: subdomainSchema,
+        required: false
     },
     info_domain: {
-     type: String,
-     required : false
+        type: String,
+        required: false
     },
     companies_domain: {
-     type: String,
-     required: false
+        type: String,
+        required: false
     },
     roles_domain: {
-        type : String,
-        required : false
+        type: String,
+        required: false
     },
-    day_in_the_life_domain : {
-        type : String,
-        required : false
+    day_in_the_life_domain: {
+        type: String,
+        required: false
     },
-    competitions_domain : {
-        type : String,
-        required : false
+    competitions_domain: {
+        type: String,
+        required: false
     },
-    internships_domain : {
-        type : String,
-        required : false
+    internships_domain: {
+        type: String,
+        required: false
     },
-    research_opportunities_domain : {
-        type : String,
-        required : false
+    research_opportunities_domain: {
+        type: String,
+        required: false
     }
-   });
+});
 
-   //Creating the collection Address
-   const Subdomain = mongoose.model('Subdomain', subdomainSchema);
-   const Domain = mongoose.model('Domain', domainSchema);
+//Creating the collection Address
+const Subdomain = mongoose.model('Subdomain', subdomainSchema);
+const Domain = mongoose.model('Domain', domainSchema);
 
-   module.exports = Domain;
-   module.exports = Subdomain;
+module.exports = { Domain, Subdomain }
